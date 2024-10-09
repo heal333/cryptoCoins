00cb9be1a15d271ff5f2f2f29f3ab120f911906b
heal333 <rohanaich333@gmail.com>
2024-10-09 18:00:43 +0530

    updating API path

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,5 +1,5 @@
-export const API = "https://server-k0iw.onrender.com";
+export const API = "https://server-k0iw.onrender.com/cryptoCoins/API";
 
-// export const API = "http://127.0.0.1:3000";
+// export const API = "http://127.0.0.1:3000/cryptoCoins/API";
 
 export const HOME_ADDRESS = "/cryptoCoins";
